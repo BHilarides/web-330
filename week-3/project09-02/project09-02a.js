86a687de19405d989e0ccbad2095dac4c9707771
Ben Hilarides <ben.hilarides26@gmail.com>
2025-06-15 18:34:23 -0400

    part 5

--- week-3/project09-02/project09-02a.js
+++ week-3/project09-02/project09-02a.js
@@ -20,3 +20,22 @@ let accOption = document.getElementById("accOption");
 let region = document.getElementById("region");
 let miles = document.getElementById("miles");
 let comments = document.getElementById("comments");
+
+// onclick button to run showData() on submit click
+document.getElementById("submitButton").onclick = showData;
+
+// showData() function
+function showData() {
+  // Store data on signup
+  sessionStorage.setItem("riderName", riderName.value);
+  sessionStorage.setItem("ageGroup", ageGroup.value);
+  sessionStorage.setItem("bikeOption", bikeOption.value);
+  sessionStorage.setItem("routeOption", routeOption.value);
+  sessionStorage.setItem("accOption", accOption.value);
+  sessionStorage.setItem("region", region.value);
+  sessionStorage.setItem("miles", miles.value);
+  sessionStorage.setItem("comments", comments.value);
+
+  // Direct to the next page
+  location.href = "project09-02b.html";
+}
